77ff7aef3c6bc1d8a03997bce6d529528639164a
salidamaharjan <salidamaharjan@outlook.com>
2024-05-03 17:45:46 -0500

    Create router for finding user by ID

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,12 +1,21 @@
 import express, { Request, Response } from "express";
 import db from "../config/db";
 import { user } from "../schema";
+import { eq } from "drizzle-orm";
 
 const userRoutes = express.Router();
 
 userRoutes.get("/users", async (req: Request, res: Response) => {
   const resultOfUsers = await db.select().from(user);
   res.status(200).json(resultOfUsers);
+});
+
+userRoutes.get("/users/:id", async (req: Request, res: Response) => {
+  const resultOfUserById = await db
+    .select()
+    .from(user)
+    .where(eq(user.id, parseInt(req.params.id)));
+  res.status(200).json(resultOfUserById);
 });
 
 export default userRoutes;
