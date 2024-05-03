73aca7051263200672477d0fc331f3fadf614cfa
salidamaharjan <salidamaharjan@outlook.com>
2024-05-03 18:00:51 -0500

    use query to find all the user and user by id

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -6,15 +6,14 @@ import { eq } from "drizzle-orm";
 const userRoutes = express.Router();
 
 userRoutes.get("/users", async (req: Request, res: Response) => {
-  const resultOfUsers = await db.select().from(user);
+  const resultOfUsers = await db.query.user.findMany();
   res.status(200).json(resultOfUsers);
 });
 
 userRoutes.get("/users/:id", async (req: Request, res: Response) => {
-  const resultOfUserById = await db
-    .select()
-    .from(user)
-    .where(eq(user.id, parseInt(req.params.id)));
+  const resultOfUserById = await db.query.user.findFirst({
+    where: eq(user.id, parseInt(req.params.id)),
+  });
   res.status(200).json(resultOfUserById);
 });
 
