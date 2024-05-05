bfca3df21a08b42c4dd6c2127f679ad0b7d44d80
salidamaharjan <salidamaharjan@outlook.com>
2024-05-05 10:53:34 -0500

    update Css

--- client/src/component/ui/Form.tsx
+++ client/src/component/ui/Form.tsx
@@ -3,24 +3,25 @@ import Input from "./Input";
 import Label from "./Label";
 
 function Form() {
+  function handleLoginCLick(){
+    alert("login")
+  }
   return (
-    <div className="flex justify-center">
-      <div className=" flex flex-col gap-4 border rounded-md p-6 min-w-[350px] items-center border-gray-500">
+    <div className="flex flex-col text-2xl font-bold text-blue-600 gap-6 items-center"> Login
+      <div className=" flex flex-col gap-6 border text-lg rounded-md p-8 sm:min-w-[250px] md:min-w-[350px] lg:min-w-[450px] items-center border-gray-500">
         <div>
           <Label className="text-gray-800">
             {" "}
-            Username: {" "}
-            <Input placeholder="username" type="text" />
+            Username: <Input placeholder="username" type="text" />
           </Label>
         </div>
         <div>
           <Label className="text-gray-800">
             {" "}
-            Password:{" "}
-            <Input placeholder="password" type="text" />
+            Password: <Input placeholder="password" type="text" />
           </Label>
         </div>
-        <Button>Login</Button>
+        <Button onClick={handleLoginCLick}>Login</Button>
       </div>
     </div>
   );
