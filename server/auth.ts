036fdc443fa784040c28abfdc316004cb3577433
salidamaharjan <salidamaharjan@outlook.com>
2024-05-06 14:18:48 -0500

    Create auth middleware

--- server/auth.ts
+++ server/auth.ts
@@ -1,8 +1,26 @@
 import jwt from "jsonwebtoken";
-import { Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
-function authMiddleware (res: Response, req: Request, next: NextFunction){
-
+function authMiddleware(res: Response, req: Request, next: NextFunction) {
+  try {
+    const authorizationHeader = req.header("Authorization");
+    if (!authorizationHeader) {
+      res.status(401).json({ message: "You are not authorized" });
+      return;
+    }
+    const token = authorizationHeader.replace("Bearer", "");
+    if (!token) {
+      res.status(401).json({ message: "Authorization token not found" });
+      return;
+    }
+    const decoded = jwt.verify(token, process.env.SECRET_KEY!);
+    (req as any).user = decoded;
+    next();
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
+
+export default authMiddleware;
