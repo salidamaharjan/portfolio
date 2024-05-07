063556a914710bca81dbb65edc8d8e4dde53e75b
salidamaharjan <salidamaharjan@outlook.com>
2024-05-07 12:58:18 -0500

    show all education in portfolio

--- client/src/pages/Portfolio.tsx
+++ client/src/pages/Portfolio.tsx
@@ -1,17 +1,45 @@
 import { get } from "../lib/http";
+import { useState, useEffect } from "react";
+
+type EducationProps = {
+  id: number;
+  degree: string;
+  schoolName: string;
+  startDate: string;
+  yearCompletion: string;
+  description: string;
+};
 function Portfolio() {
+  const [educations, setEducations] = useState<EducationProps[]>([]);
+  useEffect(() => {
+    fetchEducation();
+  }, []);
   async function fetchEducation() {
     try {
-      const educations = await get("http://localhost:3001/api/educations");
-      console.log("educations", educations);
+      const educationData = await get("http://localhost:3001/api/educations");
+      setEducations(educationData);
+      console.log("educationData", educationData);
     } catch (err) {
       console.log("err", err);
     }
   }
   return (
-    <div className="font-bold text-red-600">
-      PORTFOLIO
-      <button onClick={fetchEducation}>Click</button>
+    <div>
+      <div className="font-bold text-lg text-blue-600">Education</div>
+      <div className="text-sm ">
+        {educations.map((education) => {
+          return (
+            <div key={education.id}>
+              {" "}
+              <div className="font-bold">{education.degree}</div>
+              <li>{education.description}</li>
+              <li>{education.schoolName}</li>
+              <li>{education.startDate}</li>
+              <li>{`${education.yearCompletion === null ? "current" : ""}`}</li>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
