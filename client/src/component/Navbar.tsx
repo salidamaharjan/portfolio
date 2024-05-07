5a667bed7b6ab7cef2c7d3c503c6ae3a8ca5dfe0
salidamaharjan <salidamaharjan@outlook.com>
2024-05-06 20:14:48 -0500

    Highlight the nav option when active

--- client/src/component/Navbar.tsx
+++ client/src/component/Navbar.tsx
@@ -1,12 +1,20 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+
 type NavbarProps = {
   className?: string;
 };
 function Navbar({ className }: NavbarProps) {
+  // const [loginStyle, setLoginStyle] = useState("");
+  // const [signupStyle, setSignupStyle] = useState("");
+
   const navigate = useNavigate();
+  const location = useLocation();
   return (
-    <nav className={`flex justify-end gap-4 ${className}`}>
+    <nav className={`flex justify-end font-bold gap-4 ${className}`}>
       <div
+        className={`${
+          location.pathname === "/portfolio" ? "text-blue-600" : ""
+        }`}
         onClick={() => {
           navigate("/portfolio");
         }}
@@ -14,6 +22,7 @@ function Navbar({ className }: NavbarProps) {
         Portfolio
       </div>
       <div
+        className={`${location.pathname === "/" ? "text-blue-600" : ""}`}
         onClick={() => {
           navigate("/");
         }}
@@ -21,6 +30,7 @@ function Navbar({ className }: NavbarProps) {
         Login
       </div>
       <div
+        className={`${location.pathname === "/signup" ? "text-blue-600" : ""}`}
         onClick={() => {
           navigate("/signup");
         }}
