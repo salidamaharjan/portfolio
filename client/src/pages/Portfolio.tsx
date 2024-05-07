7229c66b0427cfc4eee72699d3398b00c12b3f53
salidamaharjan <salidamaharjan@outlook.com>
2024-05-07 13:07:39 -0500

    fetch the project in portfolio page

--- client/src/pages/Portfolio.tsx
+++ client/src/pages/Portfolio.tsx
@@ -9,16 +9,35 @@ type EducationProps = {
   yearCompletion: string;
   description: string;
 };
+
+type ProjectProps = {
+  id: number;
+  projectName: string;
+  description: string;
+  technologiesUsed: string;
+};
+
 function Portfolio() {
   const [educations, setEducations] = useState<EducationProps[]>([]);
+  const [projects, setProjects] = useState<ProjectProps[]>([]);
+
   useEffect(() => {
     fetchEducation();
+    fetchProject();
   }, []);
   async function fetchEducation() {
     try {
       const educationData = await get("http://localhost:3001/api/educations");
       setEducations(educationData);
-      console.log("educationData", educationData);
+      // console.log("educationData", educationData);
+    } catch (err) {
+      console.log("err", err);
+    }
+  }
+  async function fetchProject() {
+    try {
+      const projectData = await get("http://localhost:3001/api/projects");
+      setProjects(projectData);
     } catch (err) {
       console.log("err", err);
     }
@@ -36,6 +55,19 @@ function Portfolio() {
               <li>{education.schoolName}</li>
               <li>{education.startDate}</li>
               <li>{`${education.yearCompletion === null ? "current" : ""}`}</li>
+            </div>
+          );
+        })}
+      </div>
+      <div className="font-bold text-lg text-blue-600">Projects</div>
+      <div className="text-sm ">
+        {projects.map((project) => {
+          return (
+            <div key={project.id}>
+              {" "}
+              <div className="font-bold">{project.projectName}</div>
+              <li>{project.description}</li>
+              <li>{project.technologiesUsed}</li>
             </div>
           );
         })}
