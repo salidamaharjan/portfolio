746a01c3b03476ac478adabb8c69e3ae362528c2
salidamaharjan <salidamaharjan@outlook.com>
2024-05-10 18:22:33 -0500

    Add edit portfolio button

--- client/src/pages/Portfolio.tsx
+++ client/src/pages/Portfolio.tsx
@@ -82,46 +82,53 @@ function Portfolio() {
       </aside>
       <div>
         {location.pathname === "/portfolio" ? (
-          <div>
-            <div className="text-red-500 font-bold">Portfolio</div>
-            <div className="flex gap-2 ">
-              <Button
-                children="Add Education"
-                onClick={() => setEducationDialogOpen(true)}
-              />
-              <Dialog
-                title="Add Your Education"
-                open={educationDialogOpen}
-                onClose={() => setEducationDialogOpen(false)}
-              >
-                <EducationForm />
-              </Dialog>
+          <div className="w-[90dvw] h-[90dvh]">
+            <div className="flex justify-between">
+              <div className="text-red-500 font-bold">Portfolio</div>
+              <div className="flex justify-end pr-4">
+                <Button>Edit Portfolio</Button>
+              </div>
             </div>
-            <div className="flex gap-2 ">
-              <Button
-                children="Add Experience"
-                onClick={() => setExperienceDialogOpen(true)}
-              />
-              <Dialog
-                title="Add Your Experience"
-                open={experienceDialogOpen}
-                onClose={() => setExperienceDialogOpen(false)}
-              >
-                <ExperienceForm />
-              </Dialog>
-            </div>
-            <div className=" flex gap-2 ">
-              <Button
-                children="Add Project"
-                onClick={() => setProjectDialogOpen(true)}
-              />
-              <Dialog
-                title="Add Your Project"
-                open={projectDialogOpen}
-                onClose={() => setProjectDialogOpen(false)}
-              >
-                <ProjectForm />
-              </Dialog>
+            <div>
+              <div className="flex gap-2 ">
+                <Button
+                  children="Add Education"
+                  onClick={() => setEducationDialogOpen(true)}
+                />
+                <Dialog
+                  title="Add Your Education"
+                  open={educationDialogOpen}
+                  onClose={() => setEducationDialogOpen(false)}
+                >
+                  <EducationForm />
+                </Dialog>
+              </div>
+              <div className="flex gap-2 ">
+                <Button
+                  children="Add Experience"
+                  onClick={() => setExperienceDialogOpen(true)}
+                />
+                <Dialog
+                  title="Add Your Experience"
+                  open={experienceDialogOpen}
+                  onClose={() => setExperienceDialogOpen(false)}
+                >
+                  <ExperienceForm />
+                </Dialog>
+              </div>
+              <div className=" flex gap-2 ">
+                <Button
+                  children="Add Project"
+                  onClick={() => setProjectDialogOpen(true)}
+                />
+                <Dialog
+                  title="Add Your Project"
+                  open={projectDialogOpen}
+                  onClose={() => setProjectDialogOpen(false)}
+                >
+                  <ProjectForm />
+                </Dialog>
+              </div>
             </div>
           </div>
         ) : (
