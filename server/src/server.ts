dde12bf897531c4ec744203e2bdc3b1909d42d7c
salidamaharjan <salidamaharjan@outlook.com>
2024-05-24 08:48:15 -0500

    Correct path for deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,18 +3,17 @@ import dotenv from "dotenv";
 dotenv.config();
 import router from "./routes";
 import cors from "cors";
-import * as path from "path";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("../client/dist"));
 app.use(router);
-app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  res.sendFile("../client/dist/index.html");
 });
 
 app.listen(port, () => {
